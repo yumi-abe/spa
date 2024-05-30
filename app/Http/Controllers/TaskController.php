9c1b606b9a913b69ffa885600a3e5d9eaadc2b75
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-05-30 21:39:35 +0900

    Task一覧の実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,24 +9,15 @@ use App\Http\Requests\UpdateTaskRequest;
 class TaskController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Task一覧
      *
-     * @return \Illuminate\Http\Response
+     * @return Task[] \Illuminate\Support\Collection
      */
     public function index()
     {
-        //
+        return Task::orderByDesc('id')->get();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -50,16 +41,6 @@ class TaskController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Task $task)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
