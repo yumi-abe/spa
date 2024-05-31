c531a3a9e4f7f41886935de32f7e9a2b073665c3
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-05-31 11:23:27 +0900

    テスト完了

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Task;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Requests\TaskRequest;
 
 class TaskController extends Controller
 {
@@ -22,10 +23,10 @@ class TaskController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  Request  $request
+     * @param  TaskRequest  $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function store(StoreTaskRequest $request)
+    public function store(TaskRequest $request)
     {
         $task = Task::create($request->all());
 
@@ -49,23 +50,29 @@ class TaskController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdateTaskRequest  $request
+     * @param  \App\Http\Requests\TaskRequest  $request
      * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(UpdateTaskRequest $request, Task $task)
+    public function update(TaskRequest $request, Task $task)
     {
-        //
+        $task->title = $request->title;
+
+        return $task->update()
+        ? response()->json($task)
+        : response()->json([], 500);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Task $task)
     {
-        //
+        return $task->delete()
+        ? response()->json($task)
+        : response()->json([], 500);
     }
 }
--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\Task;
+use Facade\Ignition\Tabs\Tab;
 
 use function PHPUnit\Framework\assertJson;
 
@@ -31,7 +32,7 @@ class TaskTest extends TestCase
      *
      * @test
      */
-    public function 登録することができる()
+    public function 登録()
     {
         $data = [
             'title' => 'テスト投稿'
@@ -47,4 +48,91 @@ class TaskTest extends TestCase
 
     }
 
+        /**
+     *
+     * @test
+     */
+    public function タイトルが空の場合登録できない()
+    {
+        $data = [
+            'title' => ''
+        ];
+
+        $response = $this->postJson('api/tasks', $data);
+
+        // dd($response->json());
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors([
+                'title' => '件名は必ず指定してください。'
+            ]);
+
+    }
+
+        /**
+     *
+     * @test
+     */
+    public function タイトルが255文字の場合は登録できない()
+    {
+        $data = [
+            'title' => str_repeat('あ', 256)
+        ];
+
+        $response = $this->postJson('api/tasks', $data);
+
+        // dd($response->json());
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors([
+                'title' => '件名は、255文字以下で指定してください。'
+            ]);
+
+    }
+
+        /**
+     *
+     * @test
+     */
+    public function 更新()
+    {
+        $task = Task::factory()->create();
+
+        $task->title = '書き換え';
+
+        // dd($task);
+        $response = $this->patchJson("api/tasks/{$task->id}", $task->toArray());
+        // dd($response->json());
+
+        $response
+            ->assertOk()
+            ->assertJsonFragment($task->toArray());
+
+    }
+
+            /**
+     *
+     * @test
+     */
+    public function 削除()
+    {
+        $tasks = Task::factory()->count(10)->create();
+
+        $response = $this->deleteJson("api/tasks/1");
+
+        $response->assertOk();
+
+        $response = $this->getJson("api/tasks");
+        $response->assertJsonCount($tasks->count() -1);
+        
+
+        // $response
+        //     ->assertOk()
+        //     ->assertJsonFragment($task->toArray());
+
+    }
+
+    
 }
